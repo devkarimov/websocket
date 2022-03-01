464c353db3f0a2c129f979b90c77b802220c870f
dev.karimov <dev.karimov@gmail.com>
2022-03-02 00:31:30 +0500

    task completed

--- js/practice.js
+++ js/practice.js
@@ -1,24 +1,39 @@
-let WebSocketsURL = "wss://wss.tradernet.ru";
-let ws = new WebSocket(WebSocketsURL);
+const WebSocketsURL = "wss://wss.tradernet.ru";
+const ws = new WebSocket(WebSocketsURL);
+
 const tickersToWatchChanges = ['RSTI', 'GAZP', 'MRKZ', 'RUAL', 'HYDR', 'MRKS', 'SBER', 'FEES', 'TGKA', 'VTBR', 'ANH.US', 'VICL.US', 'BURG.US', 'NBL.US', 'YETI.US', 'WSFS.US', 'NIO.US', 'DXC.US', 'MIC.US', 'HSBC.US', 'EXPN.EU', 'GSK.EU', 'SHP.EU', 'MAN.EU', 'DB1.EU', 'MUV2.EU', 'TATE.EU', 'KGF.EU', 'MGGT.EU', 'SGGD.EU'];
-// const tickersToWatchChanges = ['EXPN.EU'];
 
 const result = document.getElementById('result');
 let list = {};
-
-/**
- * @param QuoteInfoAnswer[] data
- */
 
 function updateUI(list) {
   result.innerHTML = '';
   Object.keys(list).forEach(listKey => {
     let item = document.createElement('div');
     
-    Object.keys(list[listKey]).forEach(listItemKey => {
+    Object.keys(list[listKey]).forEach((listItemKey, index) => {
       let itemSpan = document.createElement('span');
-      itemSpan.innerHTML = list[listKey][listItemKey].data;
+
+      itemSpan.innerHTML = list[listKey][listItemKey].data + (listItemKey === 'pcp' ? '%' : '' );
       list[listKey][listItemKey].class && itemSpan.classList.add(list[listKey][listItemKey].class);
+
+      if (index === 0) {
+        let div = document.createElement('div');
+        
+        div.append(itemSpan);
+
+        item.append(div);
+
+        return;
+      }
+
+      if (index === 1) {
+        item.querySelector('div').append(itemSpan);
+
+        return;
+      }
+
+      
       item.append(itemSpan);
     })
 
@@ -27,24 +42,21 @@ function updateUI(list) {
 }
 
 function updateWatcher(data) {
-  console.log(data.c)
   if (list.hasOwnProperty(data.c)) {
-    console.log(data.c)
     Object.keys(list[data.c]).forEach(key => {
-      if (key === 'chg' || key === 'ltp' || key === 'pcp' || key === 'ltt') {
+      if (key === 'chg' || key === 'ltp' || key === 'pcp') {
         if (data[key] && list[data.c][key].data) {
           if (list[data.c][key].data > data[key]) {
             list[data.c][key].class = 'down'
           } else if (list[data.c][key].data < data[key]) {
             list[data.c][key].class = 'up'
           } else {
-            list[data.c][key].class === ''
+            list[data.c][key].class = ''
           }
 
           list[data.c][key].data = data[key]
         }
       }
-
     })
   } else {
     list[data.c] = {
@@ -52,9 +64,9 @@ function updateWatcher(data) {
         class: '',
         data: data.c
       },
-      pcp: {
+      ltp: {
         class: '',
-        data: data.pcp
+        data: data.ltp
       },
       ltr: {
         class: '',
@@ -64,17 +76,13 @@ function updateWatcher(data) {
         class: '',
         data: data.name
       },
-      ltp: {
-        class: '',
-        data: data.ltp
-      },
       chg: {
         class: '',
         data: data.chg
       },
-      ltt: {
+      pcp: {
         class: '',
-        data: data.ltt
+        data: data.pcp
       }
     }
   }
@@ -83,17 +91,12 @@ function updateWatcher(data) {
 }
 
 ws.onmessage = function (m) {
-  console.log('asdasdasdadsad')
   const [event, data] = JSON.parse(m.data);
   if (event === 'q') {
     updateWatcher(data);
   }
 };
+
 ws.onopen = function() {
-  ws.send(JSON.stringify(['quotes', tickersToWatchChanges, {
-    "type": "stocks",
-    "exchange": "russia",
-    "gainers": 0,
-    "limit": 30
-  }]));
+  ws.send(JSON.stringify(['quotes', tickersToWatchChanges]));
 }
